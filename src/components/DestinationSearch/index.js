9950b8aa0b1827d8173a0e2d7602e50695f9b07e
Dheeraj Meena <91050640+DheerajMeena98@users.noreply.github.com>
2021-10-20 00:52:33 +0530

    Update index.js

--- src/components/DestinationSearch/index.js
+++ src/components/DestinationSearch/index.js
@@ -1,19 +1,54 @@
-import DestinationItem from './components/DestinationItem'
+import {Component} from 'react'
+
+import DestinationItem from '../DestinationItem/index'
 
 import './index.css'
 
-import {Component} from 'react'
-
 class DestinationSearch extends Component {
+  state = {
+    destinationName: '',
+  }
+
+  searchDestination = event => {
+    const inputValue = event.target.value
+    this.setState({destinationName: inputValue})
+  }
+
   render() {
     const {destinationsList} = this.props
+    const {destinationName} = this.state
+
+    const searchResults = destinationsList.filter(eachDestination => {
+      const eachDestinationNameLower = eachDestination.name.toLowerCase()
+      return eachDestinationNameLower.includes(destinationName)
+    })
+
     return (
       <div className="destination-search-bcg-container">
-        <h1> Destination Search</h1>
-        <input type="search" />
-        <div className="destination-search-container">
-          <DestinationItem />
+        <h1> Destination Search </h1>
+
+        <div className="input-container">
+          <input
+            type="search"
+            onChange={this.searchDestination}
+            value={destinationName}
+            className="user-input"
+          />
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png"
+            alt="search icon"
+            className="search-icon"
+          />
         </div>
+
+        <ul className="destination-search-container">
+          {searchResults.map(eachDestination => (
+            <DestinationItem
+              eachDestination={eachDestination}
+              key={eachDestination.id}
+            />
+          ))}
+        </ul>
       </div>
     )
   }
